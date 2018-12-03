66c2bf0d3b666f03e31b87c889c9e0f19252d0c7
beef <beeflawg@gmail.com>
2018-12-03 10:10:13 -0700

    updated files

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
+
 
 // Initialize Express
 var app = express();
@@ -28,6 +29,7 @@ app.use(express.static("public"));
 
 // Connect to the Mongo DB
 mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.connect(MONGODB_URI);
